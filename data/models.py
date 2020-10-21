6696770f5e992a89f7575553e1ad1bf61d719b0a
Nicholas Spalding <nspalding@ioet.com>
2020-10-20 23:46:45 -0500

    Create Dummy Models for testing migration strategies

--- data/models.py
+++ data/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Thing(models.Model):
+    name = models.CharField(max_length=255, blank=True, default='')
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+class UnrelatedThing(models.Model):
+    name = models.CharField(max_length=255, blank=True, default='')
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+class SubThing(models.Model):
+    name = models.CharField(max_length=255, blank=True, default='')
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    thing = models.ForeignKey(Thing, null=True, on_delete=models.CASCADE)
