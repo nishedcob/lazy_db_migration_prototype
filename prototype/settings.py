4a76222e71bb0359376733fc5a3fa6ea960c0bb9
Nicholas Spalding <nspalding@ioet.com>
2020-10-20 22:57:23 -0500

    Prototype Django App should be allowed to listen on any hostname/ip address (as we do not know what IP Address Docker will always assign to the container)

--- prototype/settings.py
+++ prototype/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3r1qes%0(st34msnqf=x+(jsew@0mukobc51%fuv)j%^&oqj&u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
