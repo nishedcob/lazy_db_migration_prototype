4edcf7cb00111163b4e530ab34ca16c47b5f9102
Nicholas Spalding <nspalding@ioet.com>
2020-10-22 00:17:04 -0500

    Add String Representations for Prototype Data Models

--- data/models.py
+++ data/models.py
@@ -7,13 +7,22 @@ class Thing(models.Model):
     date_created = models.DateTimeField()
     date_modified = models.DateTimeField()
 
+    def __str__(self):
+        return f"id={self.id} name={self.name} created={self.date_created} modified={self.date_modified}"
+
 class UnrelatedThing(models.Model):
     name = models.CharField(max_length=255, blank=True, default='')
     date_created = models.DateTimeField()
     date_modified = models.DateTimeField()
+
+    def __str__(self):
+        return f"id={self.id} name={self.name} created={self.date_created} modified={self.date_modified}"
 
 class SubThing(models.Model):
     name = models.CharField(max_length=255, blank=True, default='')
     date_created = models.DateTimeField()
     date_modified = models.DateTimeField()
     thing = models.ForeignKey(Thing, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"id={self.id} name={self.name} created={self.date_created} modified={self.date_modified}"
