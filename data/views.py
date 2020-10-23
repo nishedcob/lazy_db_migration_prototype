5fedc6972d41dcd78a7ead0bff8bb33fa379192e
Nicholas Spalding <nspalding@ioet.com>
2020-10-22 22:25:33 -0500

    Django Rest Framework Read Only Model ViewSets for Models/Serializers

--- data/views.py
+++ data/views.py
@@ -1,3 +1,20 @@
+
 from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
+
+from data.models import Thing, SubThing, UnrelatedThing
+from data.serializers import ThingSerializer, SubThingSerializer, UnrelatedThingSerializer
 
 # Create your views here.
+
+class ThingViewSet(ReadOnlyModelViewSet):
+    queryset = Thing.objects.all()
+    serializer_class = ThingSerializer
+
+class SubThingViewSet(ReadOnlyModelViewSet):
+    queryset = SubThing.objects.all()
+    serializer_class = SubThingSerializer
+
+class UnrelatedThingViewSet(ReadOnlyModelViewSet):
+    queryset = UnrelatedThing.objects.all()
+    serializer_class = UnrelatedThingSerializer
