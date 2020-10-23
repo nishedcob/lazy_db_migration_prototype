4b141a4d305dad9b60b19d37d0b7400ced724b54
Nicholas Spalding <nspalding@ioet.com>
2020-10-23 00:05:39 -0500

    Add last_accessed field to Thing

--- data/models.py
+++ data/models.py
@@ -6,6 +6,7 @@ class Thing(models.Model):
     name = models.CharField(max_length=255, blank=True, default='')
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
+    last_accessed = models.DateTimeField(blank=True, null=True, default=None)
 
     def __str__(self):
         return f"id={self.id} name={self.name} created={self.date_created} modified={self.date_modified}"
